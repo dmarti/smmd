3f86a7ebf2556a12e7ea9b05328ce67876ddb095
Don Marti <dmarti@zgp.org>
2020-07-23 07:40:53 -0700

    Fix options on Docker volume for preview

--- preview.sh
+++ preview.sh
@@ -8,10 +8,11 @@ trap 'cleanup' EXIT
 pushd $PWD
 cd $(dirname "$0")
 
+docker rm -f smmd-preview &> /dev/null
 docker build -t smmd-preview . 
-docker rm -f smmd-preview
+
 docker run --name smmd-preview \
-	--volume "$(pwd)":/jekyll-site \
+	--volume "$(pwd)":/jekyll-site:rw,Z \
 	-p 4000:4000 \
 	smmd-preview 
 
